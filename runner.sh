6c2c8d7d60b4dc545cd373675c3ce42f7ef7fa48
Rodrigo Laigner <rodrigolaigner@gmail.com>
2020-02-12 16:13:15 -0300

    script to call every parameter

--- runner.sh
+++ runner.sh
@@ -9,25 +9,29 @@
 echo "Script runner.sh started"
 echo "$(date +%d)/$(date +%m)/$(date +%Y) $(date +%H):$(date +%M):$(date +%S) $(date +%Z)"
 
-FILENAME = $(echo "concurrent.c")
+FILENAME="concurrent.c"
 
-if [ ! -e ${FILENAME} ]; then
+if [ ! -e $FILENAME ]; then
     echo "ERROR: File ${FILENAME} not found"
     exit 1
 fi
 
-#COMMAND = "gcc $FILENAME.c -pthread -lm -o $FILENAME.o";
-COMMAND = $(which gcc);
+gcc concurrent.c -pthread -lm -o concurrent.o
 
-for ((i = 0; i < 8; i++)); do
-    echo "Triggering execution #${i} of ${FILENAME}"
 
-    #TODO run filename
-    echo "${COMMAND}"
+NUM_THREADS=('1' '2' '4' '8' '16' '32')
+
+for val_t in "${NUM_THREADS[@]}"; do
+    
+    for val_h in {1..18}; do
+        echo "#Threads: $val_t #Bits: $val_h"
+
+        #./concurrent.o $val_t $val_h
+
+    done
 
 done
 
-# TODO acess files and compute average
-# another c program can be called
+
 
 echo "Script runner.sh finished"
