669b26a2fb4eabc0ac97445d2859ea644388fa9e
Rodrigo Laigner <rodrigolaigner@gmail.com>
2020-02-12 16:21:17 -0300

    actually calling the C program

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script is responsible for ...
+# This script is responsible for automating the generation of average execution time of partitioning algorithms in memory. Each parameter passed to the file that hols the algorithms will generate a text file. The text file generated contains the average time.
 
 # Hints
 # https://devhints.io/bash
@@ -16,8 +16,7 @@ if [ ! -e $FILENAME ]; then
     exit 1
 fi
 
-gcc concurrent.c -pthread -lm -o concurrent.o
-
+gcc concurrent.c -pthread -lm -o concurrent.o 
 
 NUM_THREADS=('1' '2' '4' '8' '16' '32')
 
@@ -25,13 +24,11 @@ for val_t in "${NUM_THREADS[@]}"; do
     
     for val_h in {1..18}; do
         echo "#Threads: $val_t #Bits: $val_h"
-
-        #./concurrent.o $val_t $val_h
+        
+        ./concurrent.o "$val_t" "$val_h"
 
     done
 
 done
-
-
 
 echo "Script runner.sh finished"
